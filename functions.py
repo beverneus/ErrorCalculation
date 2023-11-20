4915268b3baa96e1f9123b9965e25fa67a4a781f
Beverneus <36573562+beverneus@users.noreply.github.com.>
2023-11-20 16:00:12 +0100

    add function for non-reproducable results

---
+++ functions.py
@@ -0,0 +1,13 @@
+import math
+
+def nietReproduceerbaar(values: list) -> tuple:
+    '''Takes a list of values, outputs a tuple (average value, absolute error)'''
+    
+    average = sum(values)/len(values)
+    
+    n = len(values)
+    distancesSquared = [(i - average)**2 for i in values]
+    significantError = math.sqrt((1/(n*(n-1))) * sum(distancesSquared))
+    absoluteError = 3*significantError
+    
+    return average, absoluteError
